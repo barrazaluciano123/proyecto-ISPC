64a707e40ff2a261cf3539212c1e004c010e57b4
Rodrigo Vizcarra <rodrigovizcarra1996@gmail.com>
2022-08-12 14:25:32 -0300

    Ejercicio N°2 Python terminado

--- Ejercicio2_Python/ejercicio.py
+++ Ejercicio2_Python/ejercicio.py
@@ -1,6 +1,6 @@
 """
 Crear una lista en Python denominada "Dueno2" y recorrerla con un bucle mostrando sus elementos por pantalla con excepción del DNI y el Apellido. Los elementos de la lista son:
-                    0   ,    1   ,    2   ,    3    ,     4
+
                 23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"
 
 Que representan los datos del dueño de un perro:
@@ -19,13 +19,7 @@ for i in range(len(Dueno2)):
     if Dueno2[i] == Dueno2[0] or Dueno2[i] == Dueno2[2]:
         continue
     else:
-        if Dueno2[i] == Dueno2[1]:
-            print("Nombre:", Dueno2[i])
-        elif Dueno2[i] == Dueno2[3]:
-            print("Teléfono:", Dueno2[i])
-        elif Dueno2[i] == Dueno2[4]:
-            print("Dirección:", Dueno2[i])
-    
+        print(Dueno2[i])
 
 
 print("")
