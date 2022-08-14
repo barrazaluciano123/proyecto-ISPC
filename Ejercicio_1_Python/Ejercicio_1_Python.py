a004ab260522c41f1a0a9d2a736bfb8600baec1b
unknown <nicolas.reales@mi.unc.edu.ar>
2022-08-14 06:36:16 -0300

    subiendo

---
+++ Ejercicio_1_Python/Ejercicio_1_Python.py
@@ -0,0 +1,6 @@
+print("Este es el primer ejercicio para el trabajo final de Programador en Python")
+Dueno=[28957346, "Juan", "Peréz", 4789689, "Belgrano 101"]
+if Dueno[0]>2600000:
+    print(Dueno[0])
+else:
+    print("El DNI es menor a 2600000")
